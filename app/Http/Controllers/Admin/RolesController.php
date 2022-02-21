048b44deff13bbe3e3aa45dd0016610a0145f8bb
Gaurab Halder <gaurabhalder92@gmail.com>
2022-02-21 09:30:37 +0600

    Revert "Package Create By role"
    This reverts commit 47c655be1a84b4a3afd85a4222e7ce348d169a7a.

--- app/Http/Controllers/Admin/RolesController.php
+++ app/Http/Controllers/Admin/RolesController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use App\Http\Controllers\Traits\MediaUploadingTrait;
 use App\Http\Requests\MassDestroyRoleRequest;
 use App\Http\Requests\StoreRoleRequest;
 use App\Http\Requests\UpdateRoleRequest;
@@ -11,13 +10,10 @@ use App\Models\Permission;
 use App\Models\Role;
 use Gate;
 use Illuminate\Http\Request;
-use Spatie\MediaLibrary\MediaCollections\Models\Media;
 use Symfony\Component\HttpFoundation\Response;
 
 class RolesController extends Controller
 {
-    use MediaUploadingTrait;
-
     public function index()
     {
         abort_if(Gate::denies('role_access'), Response::HTTP_FORBIDDEN, '403 Forbidden');
@@ -40,9 +36,6 @@ class RolesController extends Controller
     {
         $role = Role::create($request->all());
         $role->permissions()->sync($request->input('permissions', []));
-        if ($media = $request->input('ck-media', false)) {
-            Media::whereIn('id', $media)->update(['model_id' => $role->id]);
-        }
 
         return redirect()->route('admin.roles.index');
     }
@@ -89,17 +82,5 @@ class RolesController extends Controller
         Role::whereIn('id', request('ids'))->delete();
 
         return response(null, Response::HTTP_NO_CONTENT);
-    }
-
-    public function storeCKEditorImages(Request $request)
-    {
-        abort_if(Gate::denies('role_create') && Gate::denies('role_edit'), Response::HTTP_FORBIDDEN, '403 Forbidden');
-
-        $model         = new Role();
-        $model->id     = $request->input('crud_id', 0);
-        $model->exists = true;
-        $media         = $model->addMediaFromRequest('upload')->toMediaCollection('ck-media');
-
-        return response()->json(['id' => $media->id, 'url' => $media->getUrl()], Response::HTTP_CREATED);
     }
 }
