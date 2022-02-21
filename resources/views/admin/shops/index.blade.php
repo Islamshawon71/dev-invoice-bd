d79582ab5885514d18a6d1d2c4669a3bb400ec3f
Shaown <islamshawon1993@gmail.com>
2022-02-21 10:23:21 +0600

    Shop Add button align

--- resources/views/admin/shops/index.blade.php
+++ resources/views/admin/shops/index.blade.php
@@ -1,17 +1,20 @@
 @extends('layouts.admin')
 @section('content')
-@can('shop_create')
-    <div style="margin-bottom: 10px;" class="row">
-        <div class="col-lg-12">
-            <a class="btn btn-sm btn-success" href="{{ route('admin.shops.create') }}">
-                {{ trans('global.add') }} {{ trans('cruds.shop.title_singular') }}
-            </a>
-        </div>
-    </div>
-@endcan
 <div class="card">
     <div class="card-header">
-        {{ trans('cruds.shop.title_singular') }} {{ trans('global.list') }}
+        <div class="row">
+            <div class="col-lg-6 col-6">
+                Total <span class="count">0</span> {{ trans('cruds.shop.title_singular') }} {{ trans('global.list') }}
+            </div>
+            <div class="col-lg-6 col-6 action-button">
+                @can('shop_create')
+                    <a class="btn btn-sm btn-success float-right" href="{{ route('admin.shops.create') }}">
+                        {{ trans('global.add') }} {{ trans('cruds.shop.title_singular') }}
+                    </a>
+                @endcan
+            </div>
+        </div>
+
     </div>
 
     <div class="card-body">
